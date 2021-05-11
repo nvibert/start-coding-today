100e609fa7ec94d72e1ed1b1764aee9257804b08
Patrick Kremer <pkremer@vmware.com>
2021-05-11 16:31:35 -0500

    Added curl vCenter folder list
    Signed-off-by: Patrick Kremer <pkremer@vmware.com>

--- bash/curl-commands.sh
+++ bash/curl-commands.sh
@@ -1,3 +1,4 @@
 # curl -k -X POST -H "vmware-use-header-authn: string" -H "Authorization: Basic dXNlcm5hbWU6cGFzc3dvcmQ=" https://localhost:8989/rest/com/vmware/cis/session
 # curl -k -H "vmware-api-session-id: e4821a1f-75a1-46f1-af7b-4fcdb16dd6cb" https://localhost:8989/rest/vcenter/folder
 # curl -k -i -u cloudadmin@vmc.local:password -X POST -c token.txt https://vcenterURL/rest/com/vmware/cis/session
+# curl -k -i -b token.txt https://vcenterURL/rest/vcenter/folder`
